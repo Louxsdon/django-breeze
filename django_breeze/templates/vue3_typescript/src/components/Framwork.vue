38e89859552fcb86f4696ce3184f0a876c960659
louxsdon <louisayivi.dev@gmail.com>
2023-04-25 13:39:59 +0000

    Add Vue3 TypeScript Template

---
+++ django_breeze/templates/vue3_typescript/src/components/Framwork.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{ framework: string }>();
+</script>
+
+<template>
+  <div
+    className="px-12 py-6 border border-purple-500 text-purple-600 rounded-lg hover:scale-105 transition-all cursor-pointer duration-200"
+  >
+    <p>{{ framework }}</p>
+  </div>
+</template>
