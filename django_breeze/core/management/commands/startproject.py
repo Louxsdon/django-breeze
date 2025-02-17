04fa0347e021ce9038ec9382ea4726ae02411955
Louxsdon <louisayivi.dev@gmail.com>
2025-02-17 13:19:19 +0000

    change startproject script

--- django_breeze/core/management/commands/startproject.py
+++ django_breeze/core/management/commands/startproject.py
@@ -1,6 +1,12 @@
-from django_breeze.core.management import BaseCommand
 import argparse
 import sys
+from pathlib import Path
+from copier import run_copy
+
+from django_breeze.core.management import BaseCommand
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
+TEMPLATE_DIR = BASE_DIR / "stubs"
 
 
 class StartProjectCommand(BaseCommand):
@@ -11,14 +17,5 @@ class StartProjectCommand(BaseCommand):
     name = "startproject"
     description = "A command that delegates to the Django admin startproject command"
 
-    def add_arguments(self, parser):
-        parser.add_argument(
-            "project_args",
-            help="Django admin startproject command arguments.",
-            nargs=argparse.REMAINDER,
-        )
-
     def handle(self, args):
-        from django.core.management import execute_from_command_line
-
-        execute_from_command_line(["django-admin"] + [args.command] + args.project_args)
+        run_copy(str(TEMPLATE_DIR), "./", unsafe=True)
